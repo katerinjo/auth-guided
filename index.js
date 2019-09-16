20af6fe019fcdafa87f78188fa93b3052dbb0cae
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-09-16 10:22:29 -0700

    password stuff

--- index.js
+++ index.js
@@ -17,9 +17,11 @@ server.get('/', (req, res) => {
 });
 
 server.post('/api/register', (req, res) => {
-  let user = req.body;
+  let {username, password} = req.body;
 
-  Users.add(user)
+  const hash = bcrypt.hashSync(password);
+
+  Users.add({username, password: hash})
     .then(saved => {
       res.status(201).json(saved);
     })
@@ -34,7 +36,7 @@ server.post('/api/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
